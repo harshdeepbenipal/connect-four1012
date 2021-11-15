402796334112df67743db0d919ad100d6251aae3
jeffj4 <jeffj4@my.yorku.ca>
2021-11-15 16:58:35 -0500

    upload image working, add chip working

--- connect4.js
+++ connect4.js
@@ -1,9 +1,24 @@
-var gameboardArray = [7][6];
+var gameboardArray = new Array[7][6];
 //from left to right, bottom to top
 //empty = 0, p1 = 1, p2 = 2
 
 function playButton() {
   console.log("working");
+}
+
+function previewFile() {
+  const preview = document.querySelector('img');
+  const file = document.querySelector('input[type=file]').files[0];
+  const reader = new FileReader();
+
+  reader.addEventListener("load", function () {
+    // convert image file to base64 string
+    preview.src = reader.result;
+  }, false);
+
+  if (file) {
+    reader.readAsDataURL(file);
+  }
 }
 
 
