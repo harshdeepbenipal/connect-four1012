181e725ab909b569dbb792cee4fa8214526669fe
jeffj4 <jeffj4@my.yorku.ca>
2021-11-15 16:34:39 -0500

    add chip and firstfreerow functions

--- connect4.js
+++ connect4.js
@@ -1,4 +1,22 @@
+var gameboardArray = [7][6];
+//from left to right, bottom to top
+//empty = 0, p1 = 1, p2 = 2
 
 function playButton() {
   console.log("working");
+}
+
+
+function addChip(playernum, column) {
+  if (firstFreeRow(column) > 6) {
+    gameboardArray[column][firstFreeRow(column)] = playernum; 
+  }
+}
+
+function firstFreeRow(column) {
+  for (i = 0; i < 6; i++) {
+    if (gameboardArray[column][i] == 0) {
+      return i;
+    }
+  } 
 }
