3b4bef3556231111fa65c471b0c739a77bcd769f
harshdeepbenipal <92134221+harshdeepbenipal@users.noreply.github.com>
2021-11-20 23:49:55 -0500

    added designing Chip for two players

--- connect4.js
+++ connect4.js
@@ -10,10 +10,13 @@ var gameboardArray = []; //2D ARRAY FOR COLUMNS AND ROW CHECK
 var player1Turn = true; //VARIABLE FOR CHECKING PLAYERTURN
 
 var currentPlayerChip; // USED TO USER IMAGE FOR IMAGE SELECT
+var twoPlayer;//boolean for game mode
+var nextPlayer;//Image select if two players
 var userImageSrc;
 //from left to right, bottom to top
 //empty = 0, p1 = 1, p2 = 2
 playernum  = 1;
+amount = 1;
 function playButton() { //TAKES THE USER TO THE SELECT GAMEMODE SCREEN
   $("#startButton").css({'display' : 'none'});
   var element = document.getElementById("title1");
@@ -29,86 +32,102 @@ function mode(b){ //YO ADD COMMENTS FOR THESE TWO
 }
 function gameMode(a){
   mode('none');
-  if (a==true){
-    //alert("Player1 vs Player2");
+  var element = document.getElementById("title1");
+  if (a){
+    amount++;
+    twoPlayer = true;
+    element.innerHTML = "DESIGN YOUR CHIPS PLAYER 1";
   }else{
-    //alert("Player vs Computer");
+    twoPlayer = false;
+    element.innerHTML = "DESIGN YOUR CHIPS";
   }
-  var element = document.getElementById("title1"); //CALLS THE DESIGNCHIP FUNCTION
-  element.innerHTML = "DESIGN YOUR CHIPS";
-  designChip();
+  designChip(); //CALLS THE DESIGNCHIP FUNCTION
 }
 function designChip(){ //DESIGN CHIP FUNCTIONALLIYU
-  $("#imgselect").css({'display' : 'block'});
+  $("#imgselect1").css({'display' : 'block'});
   $("#go").css({'display' : 'block'});
 }
 
 function gameScreen(){ 
-  $("#imgselect").css({'display' : 'none'});//WHEN LETS GO BUTTON IS CLICKED STARTS UP THE GAMESCREEN
-  $("#go").css({'display' : 'none'});
-  $("#board1").css({'display' : 'block'});
-  $("#selection").css({'visibility' : 'visible'});
+  $("#imgselect1").css({'display' : 'none'});//WHEN LETS GO BUTTON IS CLICKED STARTS UP THE GAMESCREEN
   var element = document.getElementById("title1");
-  element.innerHTML = "CONNECT 4";
-  document.getElementById("title1").style.fontSize = "45px";
-  document.getElementById("title1").style.textAlign = "left";
-
-
-  for (var i = 1; i <= 42; i++){ //MAKES IMAGE FOR EVERY HOLE ON THE GAMEBOARD
-    var newImg = document.createElement("img");
-    $(newImg).attr("id", "chipimg" + i);
-    $(newImg).attr("class", "chipimgclass")
-    //add a dummy image
-    $(newImg).attr("src", "./images/white.jpg");
-    $("#board1").append(newImg);
-}
-for (var i = 0; i <= 6; i++){ //MAKES THE TRANSPARENT BUTTONS ON THE GAMEBOARD TO PLACE CHIPS ON THE EMPTY HOLES IN THE BOARD
-  var newButton = document.createElement("button");
-  $(newButton).attr("id", "column" + i);
-  $(newButton).attr("class", "columnclass")
-  //add a dummy image
-  $(newButton).html(i + 1+ " column");
-  var string = "addChip(i);";
-  $(newButton).attr("onClick", "addChip("+i+");");
-  console.log(i);
-  $("#board1").append(newButton);
-
-}
+  if (amount==2){
+    $("#imgselect2").css({'display' : 'block'});
+    element.innerHTML = "DESIGN YOUR CHIPS PLAYER 2";
+    amount++;
+  }else{
+    $("#imgselect2").css({'display' : 'none'});
+    $("#go").css({'display' : 'none'});
+    $("#board1").css({'display' : 'block'});
+    $("#selection").css({'visibility' : 'visible'});
+    element.innerHTML = "CONNECT 4";
+    document.getElementById("title1").style.fontSize = "45px";
+    document.getElementById("title1").style.textAlign = "left";
+    for (var i = 1; i <= 42; i++){ //MAKES IMAGE FOR EVERY HOLE ON THE GAMEBOARD
+      var newImg = document.createElement("img");
+      $(newImg).attr("id", "chipimg" + i);
+      $(newImg).attr("class", "chipimgclass")
+      //add a dummy image
+      $(newImg).attr("src", "./images/white.jpg");
+      $("#board1").append(newImg);
+    }
+    for (var i = 0; i <= 6; i++){ //MAKES THE TRANSPARENT BUTTONS ON THE GAMEBOARD TO PLACE CHIPS ON THE EMPTY HOLES IN THE BOARD
+      var newButton = document.createElement("button");
+      $(newButton).attr("id", "column" + i);
+      $(newButton).attr("class", "columnclass")
+      //add a dummy image
+      $(newButton).html(i + 1+ " column");
+      var string = "addChip(i);";
+      $(newButton).attr("onClick", "addChip("+i+");");
+      console.log(i);
+      $("#board1").append(newButton);
+    }
+  }
 }
 function previewFile() { //PREVIEW IMAGE AND IMAGE SELECTOR AND STORE IMAGE FOR THE PLAYER
-  const preview = document.querySelector('img');
-  const file = document.querySelector('input[type=file]').files[0];
-  const reader = new FileReader();
-
-  reader.addEventListener("load", function () {
+  const preview1 = document.querySelector('img');
+  const file1 = document.querySelector('input.pFile1[type=file]').files[0];
+  const reader1 = new FileReader();
+  reader1.addEventListener("load", function () {
     // convert image file to base64 string
-    preview.src = reader.result;
+    preview1.src = reader1.result;
 
   }, false);
-
-  if (file) {
-    reader.readAsDataURL(file);
+  if (file1) {
+    reader1.readAsDataURL(file1);
   }
   currentPlayerChip = document.querySelector('img').src;
+  if(twoPlayer){
+    const preview2 = document.querySelector('img'); //The preview doesn't work
+    const file2 = document.querySelector('input.pFile2[type=file]').files[0];
+    const reader2 = new FileReader();
+    reader2.addEventListener("load", function () {
+    // convert image file to base64 string
+    preview2.src = reader2.result;
 
+  }, false);
+  if (file2) {
+    reader2.readAsDataURL(file2);
+  }
+  nextPlayer = document.querySelector('img').src;//This works
+  }
 }
 
 
 function addChip(column) {
-
-
-
   if (firstFreeRow(column) < 6) {
     var imgIndex = (5 - firstFreeRow(column))* 7 + 1 + parseInt(column);
     if (player1Turn == true) {
       gameboardArray[column][firstFreeRow(column)] = 1; 
     } else {
       gameboardArray[column][firstFreeRow(column)] = 2; 
-
     }
   }
-  if (player1Turn == true) {
+  if (player1Turn) {
     currentPlayerChip = document.querySelector('img').src;
+  }
+  else if (!player1Turn && twoPlayer){
+    currentPlayerChip = nextPlayer;
   }
   document.getElementById("chipimg" + imgIndex).setAttribute('src', currentPlayerChip);
   console.log(gameboardArray);
@@ -256,15 +275,22 @@ function checkDiagonalWin(row, column) {
 }
 
 function turnSwitch() {
-
-  if (player1Turn == true) {
-    currentPlayerChip = "./images/dinov2.png";
-    player1Turn = false;
-  } else {
-    currentPlayerChip = document.querySelector('img').src;
-    player1Turn = true;
+  if(!twoPlayer){
+    if (player1Turn) {
+      currentPlayerChip = "./images/dinov2.png";
+      player1Turn = false;
+    } else {
+      currentPlayerChip = document.querySelector('img').src;
+      player1Turn = true;
+    }
+  }else{
+    if (player1Turn) {
+      currentPlayerChip = document.querySelector('img').src;
+      player1Turn = false;
+    }
+    else {
+      currentPlayerChip = nextPlayer;
+      player1Turn = true;
+    }
   }
-
-
-
 }
